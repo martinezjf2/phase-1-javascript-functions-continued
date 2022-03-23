ef175f37fd87fee35e532523dee0b302a073b43b
Jeffrey Martinez <martinezjf2@gmail.com>
2022-03-23 09:50:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
 // Your code here
-
-function saturdayFun(p='roller-skate') {
-    return `This Saturday, I want to ${p}!`
-}
-
-function mondayWork(p='go to the office') {
-    return `This Monday, I will ${p}.`
-}
-
-function wrapAdjective(p="*") {
-    return function(s='special') {
-        return `You are ${p}${s}${p}!`
-    }
-}
-
-//rerunning tests
