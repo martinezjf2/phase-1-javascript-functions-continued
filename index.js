faa93cf4c80335fd1221a40002dca1e36b0820fa
Jeffrey Martinez <martinezjf2@gmail.com>
2022-07-14 18:01:29 -0400

    starter code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+	return 'This Saturday, I want to ${activity}!';
+}
+
+let mondayWork = function (activity = "go to the office") {
+	return 'This Monday, I will ${activity}.';
+};
+
+let wrapAdjective = function (string, star='*') {
+	return "You are ${star} ${string}"
+};
+
+function result() {}
+
+// link to the repo: https://github.com/martinezjf2/phase-1-javascript-functions-continued/blob/fis-wip/index.js
