86296e315fa0cdb10de19b69f00d2ce3a75359e7
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-04 13:01:12 -0400

    mock session prep

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+	return 'This Saturday, I want to ${activity}!';
+}
+
+let mondayWork = function (activity = "go to the office") {
+	return 'This Monday, I will ${activity}.';
+};
+
+let wrapAdjective = function (string, star='*') {
+	return "You are ${star} ${string}"
+};
+
+function result() {}
